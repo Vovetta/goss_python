22349d58be77ae1f4f9f315c2028ffef258d03ff
Vovetta <vkovyzin@yandex.ru>
2022-10-23 19:22:56 +0300

    some more tasks

--- api/auth.py
+++ api/auth.py
@@ -1,6 +1,11 @@
 from datetime import datetime, timezone, timedelta
 
-from fastapi.responses import PlainTextResponse
+from PIL import Image
+from aiohttp import request
+from fastapi import File
+from fastapi.responses import PlainTextResponse, FileResponse
+
+IMAGE = Image.open('image.png')
 
 
 async def login():
@@ -9,3 +14,18 @@ async def login():
 
 async def hour():
     return PlainTextResponse(datetime.now(tz=timezone(timedelta(hours=3))).strftime('%H'))
+
+
+async def user(user_id):
+    async with request('GET', f'https://nd.kodaktor.ru/users/{user_id}') as response:
+        return PlainTextResponse((await response.json())['login'])
+
+
+async def decypher(key: bytes = File(), secret: bytes = File()):
+    raise NotImplementedError()
+
+
+async def make_image(height: int = 200, width: int = 100):
+    new_image = IMAGE.resize((width, height))
+    new_image.save('response.png', format='PNG')
+    return FileResponse('response.png', media_type='image/png')
--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from hypercorn.asyncio import serve, Config
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 
-from api.auth import login, hour
+from api.auth import login, hour, user, decypher, make_image
 
 app = FastAPI(routes=[
     APIRoute('/login', login, methods=['GET']),
-    APIRoute('/hour', hour, methods=['GET'])
+    APIRoute('/hour', hour, methods=['GET']),
+    APIRoute('/decypher', decypher, methods=['GET', 'POST']),
+    APIRoute('/id/{user_id}', user, methods=['GET']),
+    APIRoute('/makeimage', make_image, methods=['GET'])
 ])
 
 if __name__ == '__main__':
     config = Config()
+    config.bind = '0.0.0.0:8000'
     config.accesslog = '-'
     config.errorlog = '-'
     run(serve(app, config))  # type: ignore
